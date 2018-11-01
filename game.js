ff532cd8ea6e41b3fc6f86b32b66d23bafe50de0
Hugo Souza <hugosouza@iheartmedia.com>
2018-11-01 19:07:31 -0200

    testint movement

--- game.js
+++ game.js
@@ -20,58 +20,69 @@ const config = {
 let game = new Phaser.Game(config);
 
 function preload () {
-    this.load.spritesheet('link', '/assets/link.png', {
+    this.load.spritesheet('dude', 'assets/dude.png', {
         frameWidth: 32,
-        frameHeight: 32
+        frameHeight: 48
     });
 }
 
 let player;
 
 function create () {
-    player = this.physics.add.sprite(400, 350, 'link');
-    player.setBounce(0.2);
+    player = this.physics.add.sprite(400, 350, 'dude');
     player.setCollideWorldBounds(true);
 
     this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+    this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('link', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('dude', {
+            start: 0,
+            end: 3
+        }),
         frameRate: 10,
         repeat: -1
     });
-    
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'link', frame: 4 } ],
-        frameRate: 20
-    });
-    
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('link', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('dude', {
+            start: 5,
+            end: 8
+        }),
         frameRate: 10,
         repeat: -1
     });
 }
 
 function update() {
-    // stop any previous movement from the last frame
-    // player.body.setVelocity(0);
-  
-    // // horizontal movement
-    // if (cursors.left.isDown) {
-    //     player.body.setVelocityX(-100);
-    // } else if (cursors.right.isDown) {
-    //     player.body.setVelocityX(100);
+    player.body.setVelocity(0);
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // horizontal movement
+    if (cursors.left.isDown) {
+        player.body.setVelocityX(-160);
+        player.anims.play('left');
+    } else if (cursors.right.isDown) {
+        player.body.setVelocityX(160);
+        player.anims.play('right');
+    }
+
+    // vertical movement
+    if (cursors.up.isDown) {
+        player.body.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.body.setVelocityY(160);
+    }
+
+    // Normalize and scale the velocity so that player can't move faster along a diagonal
+    player.body.velocity.normalize().scale(160);
+    // } else {
+    //     player.setVelocityX(0);
+    //     player.setVelocityY(0);
+    //     player.anims.play('turn');
     // }
-  
-    // // vertical movement
-    // if (cursors.up.isDown) {
-    //     player.body.setVelocityY(-100);
-    // } else if (cursors.down.isDown) {
-    //     player.body.setVelocityY(100);
-    // }
-  
-    // // normalize and scale the velocity so that player can't move faster along a diagonal
-    // player.body.velocity.normalize().scale(speed);
+    // player.body.velocity.normalize().scale(160);
 }
